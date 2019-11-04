96f9884898b2562ca3e2ccec0d634900ce2815a3
Çelebi Murat <cmurat@atlassian.com>
2019-11-04 17:53:12 +0100

    Add description for the AccountingLockService

--- src/main/java/per/cmurat/other/revolut/accounting/service/AccountingLockService.java
+++ src/main/java/per/cmurat/other/revolut/accounting/service/AccountingLockService.java
@@ -4,15 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class AccountingLockService {
+/**
+ * Stores locks for the given key. It is intended to be used with {@link AccountingService}.
+ */
+class AccountingLockService {
     private final Map<String, ReentrantLock> keyLockMap = new HashMap<>();
 
-    public synchronized ReentrantLock getLock(String key) {
+    synchronized ReentrantLock getLock(String key) {
         return keyLockMap.compute(key, (k, v) -> {
             if (v == null) {
                 v = new ReentrantLock();
             }
-
             return v;
         });
     }
