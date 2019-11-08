b1bd12d387ed8e939a9a06bca24e8e1ad8093414
Çelebi Murat <cmurat@atlassian.com>
2019-11-08 02:00:07 +0100

    Remove unnecessary functionality

--- core/src/main/java/per.cmurat.other.revolut.core/accounting/model/TransactionRepository.java
+++ core/src/main/java/per.cmurat.other.revolut.core/accounting/model/TransactionRepository.java
@@ -2,40 +2,5 @@ package per.cmurat.other.revolut.core.accounting.model;
 
 import per.cmurat.other.revolut.core.db.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 public class TransactionRepository extends Repository<Transaction> {
-    private final Map<Long, List<Transaction>> storeByDebitAccount = new ConcurrentHashMap<>();
-    private final Map<Long, List<Transaction>> storeByCreditAccount = new ConcurrentHashMap<>();
-
-    @Override
-    public Transaction store(final Transaction transaction) {
-        final Transaction stored = super.store(transaction);
-        store(transaction.getDebitAccount().getId(), transaction, storeByDebitAccount);
-        store(transaction.getCreditAccount().getId(), transaction, storeByCreditAccount);
-
-        return stored;
-    }
-
-    private void store(final long key, final Transaction value, final Map<Long, List<Transaction>> store) {
-        store.compute(key, (k, v) -> {
-            if (v == null) {
-                v = new ArrayList<>();
-            }
-            v.add(value);
-
-            return v;
-        });
-    }
-
-    public List<Transaction> findByDebitAccountId(long debitAccountId) {
-        return storeByDebitAccount.get(debitAccountId);
-    }
-
-    public List<Transaction> findByCreditAccountId(long creditAccountId) {
-        return storeByCreditAccount.get(creditAccountId);
-    }
 }
